b1e78435b1fbdaaf9291a9bf0d6991eac70fe2b0
Kanykei <k.bekturova09@gmail.com>
2023-01-10 17:04:00 +0600

    changed BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, HashRouter, Switch} from "react-router-dom";
 import PostCard from './component/cards';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
   <Switch>
-    <Route exact path='/'>
+    <HashRouter exact path='/CardsWithPosts/'>
       <App />
-    </Route>
-    <Route exact path='/posts/:id'>
+    </HashRouter>
+    <HashRouter exact path='/CardsWithPosts/posts/:id'>
       <PostCard />
-    </Route>
+    </HashRouter>
   </Switch>
-  </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
